6a9584ab6a76a25d57b98c214fa8e0fe22b7ed30
Gabriella Melki <gabriellamelki@gmail.com>
2021-01-09 19:14:34 -0500

    tests: - placeholder for metrics - initial coverage for base and image dataset loading - fix for missing imports in trainer

---
+++ omni_model/tests/test_data/test_datasets.py
@@ -0,0 +1,36 @@
+import pytest
+from omni_model.src.data.datasets import BaseDataset, ImageDataset
+from omni_model.src.data.dataset_helpers import (
+    _SUPPORTED_DATASETS,
+    _TRANSFORMS,
+    _DATASET_TO_GROUP,
+)
+
+
+@pytest.fixture
+def basedataset():
+    yield BaseDataset
+
+
+class TestBaseDataset:
+    def test_default_dataset_loads(self, basedataset):
+        BaseDataset(dataset_name="CIFAR10", subset_fraction=1.0)
+
+    @pytest.mark.parametrize("subset_fraction", [1.0])
+    @pytest.mark.parametrize("dataset_name", [*list(_SUPPORTED_DATASETS.keys())])
+    def test_default_dataset(self, basedataset, dataset_name, subset_fraction):
+        dataset = BaseDataset(
+            dataset_name=dataset_name, subset_fraction=subset_fraction
+        )
+        assert dataset.dataset_root == _SUPPORTED_DATASETS[dataset_name]
+
+
+@pytest.fixture
+def imagedataset():
+    yield ImageDataset
+
+
+class TestImageDataset:
+    def test_default_imagedataset_loads(self, imagedataset):
+        dataset = ImageDataset(dataset_name="CIFAR10", subset_fraction=1.0)
+        assert dataset.transformation == _TRANSFORMS[_DATASET_TO_GROUP["CIFAR10"]]
--- omni_model/tests/test_trainer/test_trainer.py
+++ omni_model/tests/test_trainer/test_trainer.py
@@ -2,7 +2,12 @@ import pytest
 from omni_model.src.trainer.base_trainer import BaseTrainer
 from torch.nn import CrossEntropyLoss
 from torchvision import datasets
-from omni_model.src.datasets.data import transform
+from omni_model.src.data.datasets import (
+    _TRANSFORMS,
+    _DATASET_TO_GROUP,
+    _CIFAR10,
+    _TRAIN,
+)
 from omni_model.tests.test_omni_model import resnet18, optimizer
 
 
@@ -19,6 +24,8 @@ class TestBaseTrainer:
             optimizer=optimizer,
             criterion=CrossEntropyLoss(),
             data_loaders=datasets.CIFAR10(
-                root="./data", train=True, download=True, transform=transform
+                root="./data",
+                train=True,
+                transform=_TRANSFORMS[_DATASET_TO_GROUP[_CIFAR10]][_TRAIN],
             ),
         )
